54e5437207bf70f1cc17cf03f56b1a0a3420b061
Juan Gutierrez <je.gutierrezc1@uniandes.edu.co>
2026-02-23 12:16:59 -0500

    Update generar_datos.R

--- Proyecto/scripts/generar_datos.R
+++ Proyecto/scripts/generar_datos.R
@@ -2,8 +2,8 @@
 source("proyecto/src/data.r") # Carga la función que creamos
 
 # Generar 50 datos
-datos_finales <- generar_tabla_sintetica(50)
+datos_finales2 <- generar_tabla_sintetica(50)
 
 # Guardar los datos en la carpeta raw
-write.csv(datos_finales, "proyecto/data/raw/datos_simulados.csv", row.names = FALSE)
+write.csv(datos_finales2, "proyecto/data/raw/datos_simulados.csv", row.names = FALSE)
 print("Datos generados y guardados en proyecto/data/raw/")
