ee81a4009495ca773fe82565a512dfc2057b9a28
Juan Gutierrez <je.gutierrezc1@uniandes.edu.co>
2026-02-22 21:23:56 -0500

    scripts

---
+++ Proyecto/scripts/generar_datos.R
@@ -0,0 +1,9 @@
+# Archivo: proyecto/scripts/generar_datos.R
+source("proyecto/src/data.r") # Carga la función que creamos
+
+# Generar 50 datos
+datos_finales <- generar_tabla_sintetica(50)
+
+# Guardar los datos en la carpeta raw
+write.csv(datos_finales, "proyecto/data/raw/datos_simulados.csv", row.names = FALSE)
+print("Datos generados y guardados en proyecto/data/raw/")
---
+++ Proyecto/src/data.r
@@ -0,0 +1,10 @@
+# Archivo: proyecto/src/funciones.R
+generar_tabla_sintetica <- function(n) {
+  set.seed(42)
+  df <- data.frame(
+    id = 1:n,
+    valor_venda = rnorm(n, mean = 100, sd = 20),
+    fecha = seq(as.Date("2023-01-01"), by = "day", length.out = n)
+  )
+  return(df)
+}
